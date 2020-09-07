dc698a820cea77d0352118c090fa2b87d094b597
최홍희 <vvshinevv@gmail.com>
2020-09-07 22:51:27 +0900

    dispatcherSerlvet에 1개의 IOC Container 사용하기

--- src/main/java/my/spring/web/WebConfig.java
+++ src/main/java/my/spring/web/WebConfig.java
@@ -2,9 +2,8 @@ package my.spring.web;
 
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.stereotype.Controller;
 
 @Configuration
-@ComponentScan(basePackages = { "my.spring" }, useDefaultFilters = false, includeFilters = @ComponentScan.Filter(Controller.class))
+@ComponentScan(basePackages = { "my.spring" })
 public class WebConfig {
 }
